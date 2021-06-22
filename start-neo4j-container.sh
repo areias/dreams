a952908f936fda62c4d51340e8459ba9b8ce9b30
Ana Areias <ana.areias@gmail.com>
2021-06-22 12:11:51 -0300

    removed extra lines docker bash script

--- start-neo4j-container.sh
+++ start-neo4j-container.sh
@@ -14,15 +14,13 @@ docker run \
 	--publish=7474:7474 \
 	--publish=7687:7687 \
 	--rm \
-	--volume=/home/areias/Documents/kineviz/dreams/data:/data \
 	--volume=/home/areias/Documents/kineviz/dreams/plugins:/plugins \
     --volume=/home/areias/Documents/kineviz/dreams/import:/var/lib/neo4j/import  \
-    --volume=/home/areias/Documents/kineviz/dreams/code:/code \
 	--env NEO4J_AUTH=neo4j/test \
     -e NEO4J_apoc_export_file_enabled=true \
     -e NEO4J_apoc_import_file_enabled=true \
     -e NEO4J_apoc_import_file_use__neo4j__config=true \
-    -e NEO4JLABS_PLUGINS='["graph-data-science"]' \
+    -e NEO4JLABS_PLUGINS='["apoc","graph-data-science"]' \
 	--name neo4j \
 	  neo4j \
 
